f8bf2b93641de1d359508315cc0b277715b9053d
Aliaksandr Lindarenka <siamxiii@gmail.com>
2015-04-02 15:30:41 +0300

    made arrows

--- master/modules/gallery/gallery.js
+++ master/modules/gallery/gallery.js
@@ -4,17 +4,37 @@
 		restrict: "E",
 		templateUrl: "/modules/gallery/gallery.html",
 		scope: {
-			dataSourceUrl: '='
+			dataSourceUrl: '=',
+			width: '@',
+			height: '@',
 		},
 		controller: function ($scope, $http, Gallery) {
 			$scope.galleryUrl = 'http://services.edmunds-media.com/image-service/media-ed/ximm/?format=jpg:progressive&image=';
 
 			$scope.dataSource = Gallery.query();
 
-			$scope.arrowStyle = {
-				'height': '60px',
-				'width': '60px'
+			$scope.navigationStyle = {
+				'height': $scope.height + 'px',
+				'width': $scope.width + 'px'
+			}
+
+			$scope.leftArrowStyle = {
+				'height': $scope.height + 'px',
+				'width': $scope.height + 'px'
+			}
+
+			$scope.rightArrowStyle = {
+				'height': $scope.height + 'px',
+				'width': $scope.height + 'px'
+			}
+
+			$scope.shiftBackground = function (arrow) {
+				$scope[arrow]['background-position'] = -$scope.height + 'px 0';
+			}
+
+			$scope.shiftBackgroundBack = function (arrow) {
+				$scope[arrow]['background-position'] = '0 0';
+			}
 		}
 	}
-}
 });
